9de626cfb1450086c9889a92f4831079baf1ec79
Camilla Poushyk <camilla.poushyk@gmail.com>
2022-07-19 12:22:16 -0400

    commit tree 4fa5fe9a8ffb62bc97642fadc5d91ed1685c2d48

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide(num1, num2) {
+    if (num2 != 0) {
+        return num1 / num2;
+    }
+    else {
+        return NaN;
+    }
+}
+
+const square = (num1) => num1 * num1;
+
+const add = (num1, num2) => num1 + num2;
